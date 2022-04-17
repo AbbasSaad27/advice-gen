3e4d833ae395c01c2ba214b295c556d9f9aed2b3
AbbasSaad27 <xenozb@gmail.com>
2022-04-17 16:51:17 +0600

    init

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// getting elements
+const adviceNo = document.querySelector(".advice-no")
+const adviceText = document.querySelector(".advice-text")
+const btnLoad = document.querySelector(".btn-load")
+
+// function for getting the advice
+const getAdvice = async function () {
+    // awating the promise fetched by the fetch func
+    const adviceRes = await fetch("https://api.adviceslip.com/advice")
+    
+    // destructering the results from the await result
+    const {slip: {id, advice}} = await adviceRes.json()
+
+    // setting the advice no and advice text
+    adviceNo.textContent = id
+    adviceText.textContent = `"${advice}"`
+}
+
+// adding the click event listener
+btnLoad.addEventListener("click", getAdvice)
