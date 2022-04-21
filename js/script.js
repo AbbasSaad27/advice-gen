9a8bd6d76fbe52a272d292f7a61f0cd36e0acbb8
AbbasSaad27 <xenozb@gmail.com>
2022-04-21 23:00:20 +0600

    testing for pwa

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const adviceText = document.querySelector(".advice-text")
 const btnLoad = document.querySelector(".btn-load")
 
 // function for getting the advice
-const getAdvice = async function () {
+const getAdvice = async function (e) {
     // awating the promise fetched by the fetch func
     const adviceRes = await fetch("https://api.adviceslip.com/advice")
     
@@ -16,6 +16,8 @@ const getAdvice = async function () {
     // setting the advice no and advice text
     adviceNo.textContent = id
     adviceText.textContent = `"${advice}"`
+
+    console.log(e)
 }
 
 // initialization
@@ -23,3 +25,13 @@ getAdvice()
 
 // adding the click event listener
 btnLoad.addEventListener("click", getAdvice)
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    })
+    .catch(function(error) {
+      console.log('Service worker registration failed, error:', error);
+    });
+  }
